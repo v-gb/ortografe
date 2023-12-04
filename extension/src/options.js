a2627677a2495247bbfd168ac2a64ec7875c4643
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2023-12-04 17:52:23 +0100

    reduce duplication

--- extension/src/options.js
+++ extension/src/options.js
@@ -1,15 +1,13 @@
 const b = window.chrome ? chrome : browser;
 const is_manifest_v2 = false
 
+const fields = ['disable', 'disable_watch', 'color','trivial', 'debug_changes', 'debug_language'];
+
 async function saveOptions(e) {
     e.preventDefault();
-    const options = {
-	disable: document.querySelector("#disable-checkbox").checked,
-	disable_watch: document.querySelector("#disable-watch-checkbox").checked,
-	color: document.querySelector("#color-checkbox").checked,
-	trivial: document.querySelector("#trivial-checkbox").checked,
-        debug_changes: document.querySelector("#debug-changes-checkbox").checked,
-        debug_language: document.querySelector("#debug-language-checkbox").checked,
+    const options = {}
+    for (f of fields) {
+        options[f] = document.getElementById(f.replace("_", "-") + "-checkbox").checked
     }
     console.log('storing', options)
     await b.storage.local.set(options);
@@ -17,7 +15,6 @@ async function saveOptions(e) {
 
 async function restoreOptions() {
     try {
-        const fields = ['disable', 'disable_watch', 'color','trivial', 'debug_changes', 'debug_language'];
         const options =
             // b.storage.local.get always returns a promise in rewrite.js,
             // while here we only get a promise with manifest v3??
@@ -25,12 +22,10 @@ async function restoreOptions() {
                      ? new Promise((resolve) => b.storage.local.get(fields, resolve))
                      : b.storage.local.get(fields))
         console.log('loaded', options)
-        document.querySelector("#disable-checkbox").checked = options.disable || false;
-        document.querySelector("#disable-watch-checkbox").checked = options.disable_watch || false;
-        document.querySelector("#color-checkbox").checked = options.color || false;
-        document.querySelector("#trivial-checkbox").checked = options.trivial || false;
-        document.querySelector("#debug-changes-checkbox").checked = options.debug_changes || false;
-        document.querySelector("#debug-language-checkbox").checked = options.debug_language || false;
+        for (f of fields) {
+            document.getElementById(f.replace("_", "-") + "-checkbox").checked =
+                options[f] || false;
+        }
     } catch (e) {
         // any exception is debuggable in firefox, so just include it in the page itself
         document.getElementById("error").textContent = e.toString() + "\n" + e.stack + b.storage.local.get.toString();
