5d59ebfa5a72a87602ce4f98c27f1411d50cc4f6
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2023-12-22 23:52:15 +0100

    add web page
    A bit sad to commit dict_snapshot.js. It seems acceptable because:
    
    - the dict is smaller than screenshots after compression (160kB), so
      it's no worse than what we've done, and what a number of other
      people are probably doing as well
    
    - I won't check in a new one on every change, just when it seems
      worthwhile, to reduce the number of copies

---
+++ site/page.js
@@ -0,0 +1,18 @@
+function rewrite(text, elt) {
+    // ideally, we´d be a bit smarter and not rewrite everything on
+    // every single keystroke here
+    const options = {color:true, trivial:false}
+    if (table === null) {
+        table = load_dict(options);
+    }
+    elt.textContent = text
+    rewrite_under(options,table,elt);
+}
+
+let table = null
+const user_text = document.getElementById('user-text')
+const converted_text = document.getElementById('converted-text')
+rewrite(user_text.value, converted_text)
+user_text.oninput = function() {
+    rewrite(this.value, converted_text)
+}
