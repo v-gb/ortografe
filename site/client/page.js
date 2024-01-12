67e88db1d9868ed62d1df532917885af7359f4d7
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2024-01-12 15:12:24 -0500

    rework web site

--- site/client/page.js
+++ site/client/page.js
@@ -9,10 +9,25 @@ function rewrite(text, elt) {
     rewrite_under(options,table,elt);
 }
 
+function rewrite2(src, dst, color) {
+    const options = {color:color, trivial:false}
+    if (table === null) {
+        table = load_dict(options);
+    }
+    dst.innerHTML = src.innerHTML;
+    rewrite_under(options,table,dst);
+}
+
 let table = null
 const user_text = document.getElementById('user-text')
 const converted_text = document.getElementById('converted-text')
 rewrite(user_text.value, converted_text)
 user_text.oninput = function() {
     rewrite(this.value, converted_text)
+}
+for (const e of document.getElementsByClassName('to-transcribe')) {
+    const dst = document.createElement('tr');
+    dst.classList.add('transcribe-result')
+    rewrite2(e, dst, false)
+    e.parentNode.insertBefore(dst, e.nextSibling)
 }
