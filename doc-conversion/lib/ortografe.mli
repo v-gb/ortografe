6aa4c0bd8360586c651ea6ec0f96f2a518da6302
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2024-01-27 15:52:57 -0500

    lib: small refactoring

--- doc-conversion/lib/ortografe.mli
+++ doc-conversion/lib/ortografe.mli
@@ -1,7 +1,3 @@
-type 'a out =
-  | Channel : Out_channel.t -> unit out
-  | String : string out
-
 val erofa : (string, string) Hashtbl.t Lazy.t
 val rect1990 : (string, string) Hashtbl.t Lazy.t
 type options =
@@ -9,14 +5,18 @@ type options =
   ; dict : (string, string) Hashtbl.t
   }
 
+type 'a out =
+  | Channel : Out_channel.t -> unit out
+  | String : string out
 type 'a convert = ?buf:Buffer.t -> options:options -> string -> dst:'a out -> 'a
-val pure_text : _ convert
-val html : 'a convert
-val htmlz : 'a convert
-val docx : 'a convert
-val epub : 'a convert
-val doc : 'a convert
 val max_size : int ref
+
+val pure_text : _ convert
+val html : _ convert
+val htmlz : _ convert
+val docx : _ convert
+val epub : _ convert
+val doc : _ convert
 
 val convert_string : ext:string -> options:options -> string -> (string * string) option
 val convert_files : options:options -> string option -> string option -> unit
@@ -31,5 +31,6 @@ module More_markup = More_markup
 (**/**)
 module Private : sig
   val grab_from_zip : string -> string -> string
-  val join_consecutive_ish_text_nodes : (Markup.signal, 'a) Markup.stream -> (Markup.signal, 'a) Markup.stream
+  val join_consecutive_ish_text_nodes
+      : (Markup.signal, 'a) Markup.stream -> (Markup.signal, 'a) Markup.stream
 end
