360fe55c34b3daa72264437239f31f6befe7574b
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2024-02-21 20:48:54 -0500

    open Base, instead of reaching out into it piecemeal

--- dict-gen/js/dict_gen.ml
+++ dict-gen/js/dict_gen.ml
@@ -1,3 +1,5 @@
+open Base
+
 let any = Js_of_ocaml.Js.Unsafe.inject
 let string s = any (Js_of_ocaml.Js.string s)
 let bool b = any (Js_of_ocaml.Js.bool b)
@@ -8,7 +10,7 @@ let json_to_string v =
   let rec to_js = function
     | `Bool b -> bool b
     | `String s -> string s
-    | `Assoc l -> obj (List.map (fun (k, v) -> (k, to_js v)) l)
+    | `Assoc l -> obj (List.map ~f:(fun (k, v) -> (k, to_js v)) l)
   in
   Js_of_ocaml.Js.Unsafe.meth_call
     (Js_of_ocaml.Js._JSON)
@@ -17,7 +19,7 @@ let json_to_string v =
   |> Js_of_ocaml.Js.to_string
 
 let generate static rules =
-  let t1 = Sys.time () in
+  let t1 = Stdlib.Sys.time () in
   let buf = Buffer.create 1_000_000 in
   let `Stats stats =
     Dict_gen_common.Dict_gen.gen
@@ -27,7 +29,7 @@ let generate static rules =
       ~json_to_string
       (`Static static)
   in
-  let t2 = Sys.time () in
+  let t2 = Stdlib.Sys.time () in
   let sexp_str =
     let open Base in
     Sexplib.Sexp.to_string_hum [%sexp (stats : Sexp.t), (t2 -. t1 : float)]
@@ -41,25 +43,25 @@ let rules () =
     let re_url = Re.(compile (seq [ str "http"; rep (compl [set " ,"]) ])) in
     fun doc ->
     doc
-    |> Base.String.substr_replace_all
+    |> String.substr_replace_all
          ~pattern:"->"
          ~with_:"→"
     |> Re.replace re_word ~all:true ~f:(fun group ->
-           let word = Base.String.chop_prefix_exn (Re.Group.get group 0) ~prefix:"@" in
+           let word = String.chop_prefix_exn (Re.Group.get group 0) ~prefix:"@" in
            [%string "<i>%{word}</i>"])
     |> Re.replace re_url ~all:true ~f:(fun group ->
            let url = Re.Group.get group 0 in
            let display_url =
              url
-             |> Base.String.chop_prefix_if_exists ~prefix:"http://"
-             |> Base.String.chop_prefix_if_exists ~prefix:"https://"
-             |> Base.String.chop_prefix_if_exists ~prefix:"www."
-             |> Base.String.chop_suffix_if_exists ~suffix:"/"
+             |> String.chop_prefix_if_exists ~prefix:"http://"
+             |> String.chop_prefix_if_exists ~prefix:"https://"
+             |> String.chop_prefix_if_exists ~prefix:"www."
+             |> String.chop_suffix_if_exists ~suffix:"/"
            in
            [%string "<a href=\"%{url}\">%{display_url}</a>"])
   in
   rules
-  |> List.map (fun rule ->
+  |> List.map ~f:(fun rule ->
          obj [ "name", string (Dict_gen_common.Dict_gen.name rule)
              ; "doc", string (ui_doc (Dict_gen_common.Dict_gen.doc rule))
              ; "v", any rule ])
@@ -85,15 +87,15 @@ let on_message
       }
   in
   match generate static rules with
-  | exception e -> Fut.error (Jv.Error.v (Jstr.of_string (Printexc.to_string e)))
+  | exception e -> Fut.error (Jv.Error.v (Jstr.of_string (Exn.to_string e)))
   | v -> Fut.ok (k v)
 
 let rpc : type q r.
           local:bool
-          -> (q -> (r, 'b) result Fut.t)
+          -> (q -> (r, 'b) Result.t Fut.t)
           -> q
           -> (q -> _)
-          -> (r, 'b) result Fut.t
+          -> (r, 'b) Result.t Fut.t
   = fun ~local impl arg constr ->
   (* This function ensures well typedness, by tying the result in the worker
      case and in the non-worker case. *)
@@ -106,20 +108,20 @@ let rpc : type q r.
     let* event = Brr.Ev.next Brr_io.Message.Ev.message
                    (Brr_webworkers.Worker.as_target worker) in
     Brr_webworkers.Worker.terminate worker;
-    Fut.return (Brr_io.Message.Ev.data (Brr.Ev.as_type event) : (r, Jv.Error.t) result)
+    Fut.return (Brr_io.Message.Ev.data (Brr.Ev.as_type event) : (r, Jv.Error.t) Result.t)
   )
   
 let generate_in_worker (lexique_url : Jstr.t) (dict1990_url : Jstr.t) rules (n : Jv.t) =
   (* We need to run this in a worker, otherwise the loading animation doesn't actually
    * animate, which we kind of want it to, since the a 2s of waiting is on the longer
    * side. *)
-  let rules = Jv.to_list (Obj.magic : Jv.t -> Dict_gen_common.Dict_gen.rule) rules in
+  let rules = Jv.to_list (Stdlib.Obj.magic : Jv.t -> Dict_gen_common.Dict_gen.rule) rules in
   let n = Jv.to_int n in
   Fut.to_promise
     ~ok:(fun (dict, duration) -> Jv.of_jv_list [ Jv.of_string dict ; Jv.of_string duration ])
     (rpc
        ~local:(n = 0)
-       (on_message ~k:Fun.id)
+       (on_message ~k:Fn.id)
        (lexique_url, dict1990_url, rules)
        (fun arg -> `On_message arg))
 
@@ -132,7 +134,7 @@ let () =
        let data = Brr_io.Message.Ev.data (Brr.Ev.as_type event) in
        match data with
        | `On_message data -> on_message data ~k:Jv.repr) 
-      (fun res -> Brr_webworkers.Worker.G.post (res : (Jv.t, Jv.Error.t) result))
+      (fun res -> Brr_webworkers.Worker.G.post (res : (Jv.t, Jv.Error.t) Result.t))
   else
     Js_of_ocaml.Js.export "dict_gen"
       (any
