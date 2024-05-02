5b961f7b607e132b4b046767eb61301d8f770ae9
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2024-05-02 14:09:44 +0200

    refactor tests

--- dict-gen/test/dict_gen_tests.ml
+++ dict-gen/test/dict_gen_tests.ml
@@ -35,42 +35,48 @@ let embedded : Dict_gen_common.Dict_gen.embedded =
   ; extension_dict1990_gen_csv = Ortografe.extension_dict1990_gen_csv
   }  
   
-let%expect_test _ =
-  let convert ~rules ~which_dict str =
-    let rules =
-      List.map rules
-        ~f:(fun elt ->
-          Dict_gen_common.Dict_gen.of_name elt
-          ||? failwith ("can't find " ^ elt))
-    in
-    let dict, metadata =
-      match which_dict with
-      | `All_at_once ->
-         let b = Buffer.create 100 in
-         let `Stats _ =
-           Dict_gen_common.Dict_gen.gen
-             ~rules
-             ~all:false
-             ~output:(Buffer.add_string b)
-             ~json_to_string:Yojson.to_string
-             (`Embedded embedded)
-         in
-         Dict_gen_common.Dict_gen.parse
-           ~json_of_string:Yojson.Basic.from_string
-           (Buffer.contents b)
-      | `Staged ->
-         Dict_gen_common.Dict_gen.staged_gen (`Embedded embedded)
-           rules
-    in
-    Ortografe.pure_text
-      ~dst:String
-      ~options:{ convert_uppercase = true
-               ; dict
-               ; interleaved = true
-               ; plurals_in_s = metadata.plurals_in_s ||? true
-               }
-      str
+let convert ~rules ~which_dict str =
+  let rules =
+    List.map rules
+      ~f:(fun elt ->
+        Dict_gen_common.Dict_gen.of_name elt
+        ||? failwith ("can't find " ^ elt))
   in
+  let dict, metadata =
+    match which_dict with
+    | `All_at_once ->
+       let b = Buffer.create 100 in
+       let `Stats _ =
+         Dict_gen_common.Dict_gen.gen
+           ~rules
+           ~all:false
+           ~output:(Buffer.add_string b)
+           ~json_to_string:Yojson.to_string
+           (`Embedded embedded)
+       in
+       Dict_gen_common.Dict_gen.parse
+         ~json_of_string:Yojson.Basic.from_string
+         (Buffer.contents b)
+    | `Staged ->
+       Dict_gen_common.Dict_gen.staged_gen (`Embedded embedded)
+         rules
+  in
+  Ortografe.pure_text
+    ~dst:String
+    ~options:{ convert_uppercase = true
+             ; dict
+             ; interleaved = true
+             ; plurals_in_s = metadata.plurals_in_s ||? true
+             }
+    str
+
+let check_and_compare str rules =
+  let all_at_once = convert ~rules ~which_dict:`All_at_once str in
+  let staged = convert ~rules ~which_dict:`Staged str in
+  print_endline all_at_once;
+  print_endline (diff_strings all_at_once staged)
+
+let%expect_test _ =
   let str = {|
 rien: rien
 érofa: rythme apparaitre
@@ -80,12 +86,7 @@ oe: oedipien oedipienne
 œ: œdipien œdipienne
 cas difficile: chariot chariotage
 |} in
-  let check_and_compare rules =
-    let all_at_once = convert ~rules ~which_dict:`All_at_once str in
-    let staged = convert ~rules ~which_dict:`Staged str in
-    print_endline all_at_once;
-    print_endline (diff_strings all_at_once staged)
-  in
+  let check_and_compare rules = check_and_compare str rules in
   let () = check_and_compare [ "erofa" ] in
   [%expect {|
     rien: rien
