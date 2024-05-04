642953a85ef56dcb9e9e5400fce3deeed3ffdfc9
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2024-05-04 15:57:58 +0200

    refactor ppx_list_include, add doc

---
+++ libs/ppx_list_include/README.md
@@ -0,0 +1,56 @@
+% ppx\_list\_include
+
+# What it does
+
+`ppx_list_include` extends the syntax of list literals with a "splice" operator:
+for instance, `[ 1; +l; 3 ]` is preprocessed into `List.concat [ [1]; l; [3] ]`.
+
+# Why
+
+This is just a minor syntactic shortcut. Sometimes, like building some
+datastructures or process arguments, list literals can feel rather limited
+because you frequently need to splice lists into lists, and starting with a list
+and turning it into a `List.concat` or similar later adds a bit of friction.
+
+For instance:
+
+```ocaml
+Process.run
+    ~prog:"diff"
+    ~args:[ "--"; file1; file2 ]
+    ()
+```
+
+when you want to pass more options:
+
+```ocaml
+Process.run
+    ~prog:"diff"
+    ~args:(List.concat
+            [ (match unified with
+              | None -> []
+              | Some ctx -> [ "-U=" ^ Int.to_string ctx ])
+            ; if side_by_side then ["-y"] else []
+            ; [ "--"
+              ; file1
+              ; file2
+              ]
+            ])
+    ()
+```
+
+Instead, with this ppx, you can write:
+
+```ocaml
+Process.run
+    ~prog:"diff"
+    ~args:[ +(match unified with
+              | None -> []
+              | Some ctx -> [ "-U=" ^ Int.to_string ctx ])
+          ; +(if side_by_side then ["-y"] else [])
+          ; "--"
+          ; file1
+          ; file2
+          ]
+    ()
+```
--- libs/ppx_list_include/src/ppx_list_include.ml
+++ libs/ppx_list_include/src/ppx_list_include.ml
@@ -35,19 +35,12 @@ let list_literal e =
         | l ->
            let loc = e.pexp_loc in
            let e = Ast_builder.Default.elist ~loc l in
+           (* Maybe we should use Base.List.concat, because Stdlib.List.concat
+              is not tail rec, and rebuilds the last element of the list because
+              it uses Stdlib.(@) which fails to optimize l @ [], unlike Base.(@). *)
            [%expr Stdlib.List.concat [%e e]])
     |> Option.some
   else None
-
-let rec list_literal_fallback rec_ e =
-  match e with
-  | { pexp_desc = Pexp_construct ({ txt = Lident "::"; _ } as id, Some ({ pexp_desc = Pexp_tuple [ a; b ]; _ } as e2)); _ } as e1 ->
-     let a' = rec_ a in
-     let b' = list_literal_fallback rec_ b in
-     if a == a' && b == b'
-     then e
-     else { e1 with pexp_desc = Pexp_construct (id, Some { e2 with pexp_desc = Pexp_tuple [ a'; b' ] }) }
-  | _ -> rec_ e
 
 let () =
   Driver.register_transformation
@@ -56,11 +49,29 @@ let () =
       object (self)
         inherit Ppxlib_traverse_builtins.map
         inherit map as super
+
+        method expression_skip_cons e =
+          (* Skip over the conses to avoid a quadratic complexity in the length of list
+             literals *)
+          match e with
+          | { pexp_desc = Pexp_construct
+                            ({ txt = Lident "::"; _ } as id,
+                             Some ({ pexp_desc = Pexp_tuple [ a; b ]; _ } as e2)); _ } ->
+             let a' = self#expression a in
+             let b' = self#expression_skip_cons b in
+             if a == a' && b == b'
+             then e
+             else { e with pexp_desc =
+                             Pexp_construct
+                               (id,
+                                Some { e2 with pexp_desc = Pexp_tuple [ a'; b' ] }) }
+          | _ -> self#expression e
+
         method! expression e =
           match e with
           | [%expr ([%e? _] :: [%e? _])] ->
              (match list_literal e with
-              | None -> list_literal_fallback self#expression e
+              | None -> self#expression_skip_cons e
               | Some e -> super#expression e)
           | _ -> super#expression e
       end#structure
