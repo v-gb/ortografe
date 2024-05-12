35b0763499ca80ca949c64236f1fffafc0395bd9
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2024-05-12 22:32:07 +0200

    add playground for trying out ocaml-built javascript cheaply

---
+++ playground/x.ml
@@ -0,0 +1,17 @@
+open Base
+type json =
+  [ `Array of json list
+  | `Assoc of (string * json) list
+  | `Boolean of bool
+  | `Null
+  | `Number of float
+  | `String of string ]
+[@@deriving sexp_of]
+
+let () =
+  let json = Brrex.json_of_string {|
+{ "a": [ "2", null, true, false, -2.34, { "a" : 2, "b" : 3 } ] }
+|} in
+  Stdlib.print_endline (Sexp.to_string_hum
+                   (sexp_of_json json));
+  ignore (Brrex.json_of_string "{")
