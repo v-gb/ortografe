094d69438de76684637e04724d2c76cb051619c5
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2024-05-28 21:33:13 +0200

    sexp_with_utf8: add test showing bug

---
+++ libs/sexp_with_utf8/test/test.ml
@@ -0,0 +1,6 @@
+open Core
+let%expect_test _ =
+  print_endline (Sexp_with_utf8.to_string_hum (Atom "accentué"));
+  [%expect {| accentué |}];
+  print_endline (Sexp_with_utf8.to_string_hum (Atom "et là"));
+  [%expect {| et là |}]
