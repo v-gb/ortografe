8755eee3891ee06fe2aa36848f5ab6bc1d310b23
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2024-06-24 13:00:19 +0200

    ppx_partial: refactor by unlining main rewriting code

--- libs/ppx_partial/src/ppx_partial.ml
+++ libs/ppx_partial/src/ppx_partial.ml
@@ -19,11 +19,68 @@ let bind expr f =
 
 let willing_to_reexecute e =
   (* Things that incur no side effects, and no allocations (to avoid transforming one
-     allocation into arbitrarily many, so no pexp_function or pexp_tuple of trivial things
-     for instance).  We must accept __, otherwise this will never fire. *)
+     allocation into arbitrarily many, so no pexp_function or pexp_tuple of trivial
+     things for instance).  We must accept __, otherwise this will never fire. *)
   match e with
   | { pexp_desc = (Pexp_ident _ | Pexp_constant _ | Pexp_construct (_, None)); _ } -> true
   | _ -> false
+
+let rewrite ~loc f params =
+  if willing_to_reexecute f
+     && List.for_all (fun (_, e) -> willing_to_reexecute e) params
+  then
+    (* generate cleaner code for trivial things like ~f:(__ + 1), to guarantee good
+       perf even if the compiler wouldn't optimize away our local function, as is
+       presumably the case in the bytecode -> js_of_ocaml pipeline *)
+    let args =
+      List.map (fun (arg, e) ->
+          match e with
+          | [%expr __] -> (arg, partial_evar ~loc:e.pexp_loc 1)
+          | _ -> (arg, e)) params
+    in
+    eabstract ~loc [partial_pvar ~loc 1]
+      (pexp_apply ~loc f args)
+  else
+    bind f (fun f ->
+        let fun_ =
+          let args =
+            List.mapi (fun i (arg, e) ->
+                arg, partial_evar ~loc:e.pexp_loc i
+              ) params in
+          let params =
+            let last_param = ref None in
+            let first_params =
+              let i = ref (-1) in
+              List.filter_map (fun (_arg, e) ->
+                  i := !i + 1;
+                  let param = partial_pvar ~loc:e.pexp_loc !i in
+                  match e with
+                  | [%expr __] -> last_param := Some param; None
+                  | _ -> Some param
+                ) params
+            in
+            first_params
+            @ (match !last_param with
+               | None -> assert false
+               | Some p -> [p])
+          in
+          eabstract ~loc
+            params
+            (pexp_apply ~loc f args)
+        in
+        let new_fun_call =
+          let remaining_params =
+            List.filter_map (function
+                | (_, [%expr __]) -> None
+                | (_, e) -> Some e) params
+          in
+          eapply ~loc
+            (evar ~loc "_partial_tmpfun")
+            remaining_params
+        in
+        [%expr
+         let _partial_tmpfun = [%e fun_] in
+             [%e new_fun_call]])  
 
 let () =
   Driver.register_transformation
@@ -84,64 +141,7 @@ let () =
                      ) params
                  in
                  { e with pexp_desc = Pexp_apply (f, params) }
-               else
-                 if willing_to_reexecute f
-                 && List.for_all (fun (_, e) -> willing_to_reexecute e) params
-                 then
-                   (* generate cleaner code for trivial things like ~f:(__ + 1), to
-                      guarantee good perf even if the compiler wouldn't optimize away
-                      our local function, as is presumably the case in the bytecode ->
-                      js_of_ocaml pipeline *)
-                   let args =
-                     List.map (fun (arg, e) ->
-                         match e with
-                         | [%expr __] -> (arg, partial_evar ~loc:e.pexp_loc 1)
-                         | _ -> (arg, e)) params
-                   in
-                   eabstract ~loc:e.pexp_loc [partial_pvar ~loc:e.pexp_loc 1]
-                     (pexp_apply ~loc:e.pexp_loc f args)
-                 else
-                   bind f (fun f ->
-                       let fun_ =
-                         let args =
-                           List.mapi (fun i (arg, e) ->
-                               arg, partial_evar ~loc:e.pexp_loc i
-                             ) params in
-                         let params =
-                           let last_param = ref None in
-                           let first_params =
-                             let i = ref (-1) in
-                             List.filter_map (fun (_arg, e) ->
-                                 i := !i + 1;
-                                 let param = partial_pvar ~loc:e.pexp_loc !i in
-                                 match e with
-                                 | [%expr __] -> last_param := Some param; None
-                                 | _ -> Some param
-                               ) params
-                           in
-                           first_params
-                           @ (match !last_param with
-                              | None -> assert false
-                              | Some p -> [p])
-                         in
-                         eabstract ~loc:e.pexp_loc
-                           params
-                           (pexp_apply ~loc:e.pexp_loc f args)
-                       in
-                       let new_fun_call =
-                         let remaining_params =
-                           List.filter_map (function
-                               | (_, [%expr __]) -> None
-                               | (_, e) -> Some e) params
-                         in
-                         eapply ~loc:e.pexp_loc
-                           (evar ~loc:e.pexp_loc "_partial_tmpfun")
-                           remaining_params
-                       in
-                       let loc = e.pexp_loc in
-                       [%expr
-                        let _partial_tmpfun = [%e fun_] in
-                            [%e new_fun_call]])
+               else rewrite ~loc:e.pexp_loc f params
           | _ -> e
        end#structure str
     )
